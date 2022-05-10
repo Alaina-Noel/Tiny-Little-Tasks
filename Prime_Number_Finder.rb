3f057d1a68c50f1f1ee8aef78aa1aaf4ac17ea07
Alaina Noel <Alaina.Noel.8@gmail.com>
2022-05-09 18:46:17 -0600

    Fixed the problem. I needed to clone the original array since I was deleting a non-prime element from that array. This meant the index remained the same and the element after the deleted element was skipped over. Gritty detail of how Ruby iterates over an array.

--- Prime_Number_Finder.rb
+++ Prime_Number_Finder.rb
@@ -1,20 +1,15 @@
 array = [17, 31, 12, 38, 10]
+result = array.clone
 
 array.each do |num|
-  p num
   checker = num - 1
-    while checker > 1
-      p "#{num} #{checker} #{array}"
-      if num % checker == 0
-        puts "#{num} & #{checker}"
-        puts "#{num % checker}"
-        array.delete(num)
-        p array
-        break
-      end
-      checker -= 12
+  while checker > 1
+    if num % checker == 0
+      result.delete(num)
+      break
     end
-
+    checker -= 1
+  end
 end
 
-p array
+p result
