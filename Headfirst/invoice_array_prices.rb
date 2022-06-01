9869eaf245dec2db1be90c13599599f8e00fb648
Alaina Kneiling <alaina.noel.8@gmail.com>
2022-06-01 10:20:44 -0600

    Added refund method

--- Headfirst/invoice_array_prices.rb
+++ Headfirst/invoice_array_prices.rb
@@ -11,4 +11,18 @@ def total(array)
     formatted_price
 end
 
+def refund(array)
+    balance = 0
+    i = 0
+    while i < array.length
+        balance -= array[i]
+        i += 1
+    end
+    formatted_balance = format("%.2f", balance)
+    formatted_balance
+end
+
+
+
 p total(prices)
+p refund(prices)
