8011d651ca6d8c66b140ff0f2c796ed581f93e67
Alaina Kneiling <alaina.noel.8@gmail.com>
2022-06-04 14:22:06 -0600

    Added .map feature

--- Headfirst/movie_reviews_project.rb
+++ Headfirst/movie_reviews_project.rb
@@ -5,18 +5,15 @@ File.open("Headfirst/reviews.txt") do |review_file|
 end
 
 relevent_lines = []
-
 relevent_lines = lines.find_all {|line| line.include?("Truncated")}
-
 reviews = relevent_lines.reject {|line| line.include?("--")}
 
-review = relevent_lines.first
-
-def return_adjective(string)
+def find_adjective(string)
     words = string.split(" ")
     index = words.find_index("is")
     words[index + 1]
 end
 
-adjective = return_adjective(reviews.first)
-p adjective
+adjectives = reviews.map{|review| find_adjective(review)}
+
+puts adjectives
