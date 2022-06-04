0e627b8009f88f83354ce4a465fc8e2e2c79cb15
Alaina Kneiling <alaina.noel.8@gmail.com>
2022-06-04 15:00:47 -0600

    Commented each line so I would be able to articulate what is going on

--- Headfirst/movie_reviews_project.rb
+++ Headfirst/movie_reviews_project.rb
@@ -1,20 +1,33 @@
-lines = []
 
+#this is an empty array called lines from txt file. At this point it contains no elements
+lines_from_txt_file = []
+
+#This is a method open called on File which is a class (I think) & I've asked AtomRunner
+#to optn a file with a title review.txt & read each line and store it in an array called lines.
 File.open("Headfirst/reviews.txt") do |review_file|
-    lines = review_file.readlines
+    lines_from_txt_file = review_file.readlines
 end
 
-relevent_lines = []
-relevent_lines = lines.find_all {|line| line.include?("Truncated")}
-reviews = relevent_lines.reject {|line| line.include?("--")}
+#At this point the array includes 8 elements where each element is a string.
 
+
+#Here I delcare an empty array called lines including truncated
+lines_including_truncated = lines_from_txt_file.find_all {|line| line.include?("Truncated")}
+relevent_lines = lines_including_truncated.reject {|line| line.include?("--")}
+
+#Now relevent lines is an array with 3 items. We have gotten rid of unnecesary elements
+
+#Here I'm defining an array that takes a string. It will split that string up by spaces and
+#check for the word is. If it finds the word is it will return the next word after is.
 def find_adjective(string)
     words = string.split(" ")
     index = words.find_index("is")
     words[index + 1]
 end
 
-adjectives = reviews.map{|review| adjective = find_adjective(review)
-    " \"#{adjective.capitalize}\""}
+#Here the lines 22-26 are not being used at all they have simply been defined.
 
-puts adjectives
+
+adjectives = relevent_lines.map{|review| adjective = find_adjective(review)
+    " \"#{adjective.capitalize}\""}
+ puts adjectives
