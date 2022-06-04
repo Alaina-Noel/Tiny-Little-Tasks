996d1bed3f23e553174ac8c6dfaa7c8d35554b97
Alaina Kneiling <alaina.noel.8@gmail.com>
2022-06-04 11:37:25 -0600

    Accept lines with movie title & reject lines with bylines

--- Headfirst/movie_reviews_project.rb
+++ Headfirst/movie_reviews_project.rb
@@ -8,8 +8,6 @@ relevent_lines = []
 
 relevent_lines = lines.find_all {|line| line.include?("Truncated")}
 
+reviews = relevent_lines.reject {|line| line.include?("--")}
 
-puts lines.length
-puts lines
-
-p relevent_lines
+puts reviews
