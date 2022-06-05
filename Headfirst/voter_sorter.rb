c91d6de3690105936d22debe0ef056f09fb0c2da
Alaina Kneiling <alaina.noel.8@gmail.com>
2022-06-05 12:04:41 -0600

    Changed literal hash to Hash.new so I could use fewer lines of code to account for nil values in the hash

--- Headfirst/voter_sorter.rb
+++ Headfirst/voter_sorter.rb
@@ -4,4 +4,11 @@ File.open("Headfirst/votes.txt") do |file|
     lines = file.readlines
 end
 
-puts lines
+votes = Hash.new(0)
+
+lines.each do |line|
+    name = line.chomp
+    votes[name] += 1 if votes[name]
+end
+
+p votes
